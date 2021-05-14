b5f8cb7556fe570e6ede5551a0be85eccd3b394e
Nana <72888369+nanaama527@users.noreply.github.com>
2021-05-14 16:29:18 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    new_student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to new_student_path
+  end
+
+  private
+
+  def set_students
+    @students = Student.all
+  end
+
 end
